7a2cc36e992f42e60596e26f28003e161863a41c
WillBlackney <44806865+WillBlackney@users.noreply.github.com>
2019-05-01 21:46:54 +0200

    Initial commit

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Engine.Models
+{
+    public class Player : BaseNotificationClass
+    {
+        private string name;
+        private string characterClass;
+        private int hitPoints;
+        private int experiencePoints;
+        private int level;
+        private int gold;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged(nameof (Name));
+            }
+        }
+        public string CharacterClass
+        {
+            get { return characterClass; }
+            set
+            {
+                characterClass = value;
+                OnPropertyChanged(nameof(CharacterClass));
+            }
+        }
+        public int HitPoints
+        {
+            get { return hitPoints; }
+            set
+            {
+                hitPoints = value;
+                OnPropertyChanged(nameof(HitPoints));
+            }
+        }
+        public int ExperiencePoints
+        {
+            get { return experiencePoints; }
+            set
+            {
+                experiencePoints = value;
+                OnPropertyChanged(nameof(ExperiencePoints));
+            }
+        }
+        public int Level
+        {
+            get { return level; }
+            set
+            {
+                level = value;
+                OnPropertyChanged(nameof(Level));
+            }
+        }
+        public int Gold
+        {
+            get { return gold; }
+            set
+            {
+                gold = value;
+                OnPropertyChanged(nameof(Gold));
+            }
+        }
+
+        public ObservableCollection<GameItem> Inventory { get; set; }
+
+        public Player()
+        {
+            Inventory = new ObservableCollection<GameItem>();
+        }
+       
+    }
+}
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Engine.Models;
+using Engine.Factories;
+
+namespace Engine.ViewModels
+{
+    public class GameSession :BaseNotificationClass
+    {
+        private Location currentLocation;
+
+        public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation
+        {
+            get { return currentLocation; }
+            set
+            {
+                currentLocation = value;
+                OnPropertyChanged(nameof (CurrentLocation));
+                OnPropertyChanged(nameof(HasLocationToNorth));
+                OnPropertyChanged(nameof(HasLocationToSouth));
+                OnPropertyChanged(nameof(HasLocationToEast));
+                OnPropertyChanged(nameof(HasLocationToWest));
+            }
+        }
+        public World CurrentWorld { get; set; }
+
+        public bool HasLocationToNorth
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1) != null;
+            }
+        }
+        public bool HasLocationToSouth
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate -1 ) != null;
+            }
+        }
+        public bool HasLocationToEast
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate) != null;
+            }
+        }
+        public bool HasLocationToWest
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate) != null;
+            }
+        }
+
+        
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player
+            {
+                Name = "Will",
+                CharacterClass = "Fighter",
+                HitPoints = 10,
+                Gold = 1000000,
+                ExperiencePoints = 0,
+                Level = 1
+            };
+
+
+                      
+
+            //WorldFactory factory = new WorldFactory();
+            CurrentWorld = WorldFactory.CreateWorld();
+
+            CurrentLocation = CurrentWorld.LocationAt(0, -1);
+
+            //CurrentPlayer.Inventory.Add(ItemFactory.CreateGameItem(1001));
+        }
+
+        public void MoveNorth()
+        {
+            if (HasLocationToNorth)
+            {
+                CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
+            }
+            
+        }
+
+        public void MoveSouth()
+        {
+            if (HasLocationToSouth)
+            {
+                CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+            }
+            
+        }
+
+        public void MoveEast()
+        {
+            if (HasLocationToEast)
+            {
+                CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
+            }
+            
+        }
+
+        public void MoveWest()
+        {
+            if (HasLocationToWest)
+            {
+                CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
+            }
+            
+        }
+    }
+}
